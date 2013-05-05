475b9a8cbc08a00f390f1a950e5804ffc1d8905b
Daniel Brain <github@biboop.com>
2013-05-06 06:29:05 +1000

    Add authorization to api

--- api.go
+++ api.go
@@ -1,4 +1,10 @@
 package biboop
 
 import (
+  "github.com/dbrain/soggy"
+  "net/http"
 )
+
+func ApiMe(ctx* soggy.Context) (int, interface{}) {
+  return http.StatusOK, ctx.Env["user"]
+}
--- biboop.go
+++ biboop.go
@@ -3,11 +3,17 @@ package biboop
 import (
   "appengine"
   "appengine/user"
+  "appengine/urlfetch"
   "github.com/dbrain/soggy"
+  "strings"
+  "net/http"
+  "encoding/json"
+  "io/ioutil"
+  "strconv"
 )
 
-type AppEngineMiddleware struct {}
-func (middleware *AppEngineMiddleware) Execute(ctx *soggy.Context) {
+type AppEngineWebMiddleware struct {}
+func (middleware *AppEngineWebMiddleware) Execute(ctx *soggy.Context) {
   aeCtx := appengine.NewContext(ctx.Req.Request)
   ctx.Env["aeCtx"] = aeCtx
 
@@ -17,6 +23,58 @@ func (middleware *AppEngineMiddleware) Execute(ctx *soggy.Context) {
   }
 
   ctx.Next(nil)
+}
+
+type AppEngineApiMiddleware struct{}
+func (middleware *AppEngineApiMiddleware) Execute(ctx *soggy.Context) {
+  aeCtx := appengine.NewContext(ctx.Req.Request)
+  urlfetchClient := urlfetch.Client(aeCtx)
+  ctx.Env["aeCtx"] = aeCtx
+  ctx.Env["urlfetchClient"] = urlfetchClient
+
+  authHeader := ctx.Req.Request.Header.Get("Authorization");
+  if strings.HasPrefix(authHeader, "Bearer ") {
+    user := loadUserDetails(ctx, authHeader, urlfetchClient)
+    if user != nil {
+      ctx.Env["user"] = user
+      ctx.Next(nil)
+    }
+  } else {
+    sendAuthFailure(ctx.Res, http.StatusUnauthorized, "unauthorized")
+  }
+}
+
+func loadUserDetails(ctx *soggy.Context, authHeader string, urlfetchClient *http.Client) map[string]interface{} {
+  req, _ := http.NewRequest("GET", "https://www.googleapis.com/oauth2/v3/userinfo?alt=json", nil)
+  req.Header.Add("Authorization", authHeader)
+  resp, err := urlfetchClient.Do(req)
+  if err != nil || resp.StatusCode != http.StatusOK {
+      sendAuthFailure(ctx.Res, http.StatusUnauthorized, "authorization_failed")
+  } else {
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+      sendAuthFailure(ctx.Res, http.StatusUnauthorized, "authorization_parse_failed")
+    } else {
+      var user map[string]interface{}
+      err := json.Unmarshal(body, &user)
+      if err != nil {
+        sendAuthFailure(ctx.Res, http.StatusUnauthorized, "authorization_body_parse_failed")
+      } else {
+        return user
+      }
+    }
+  }
+  return nil
+}
+
+func sendAuthFailure(res *soggy.Response, status int, reason string) {
+  res.Set("Content-Type", "application/json; charset=utf-8")
+  jsonOut, err := json.Marshal(map[string]interface{} { "error": reason })
+  if err == nil {
+    res.Set("Content-Length", strconv.Itoa(len(jsonOut)))
+    res.WriteHeader(status)
+    _, err = res.Write(jsonOut)
+  }
 }
 
 func startWebServer() *soggy.Server {
@@ -30,20 +88,22 @@ func startWebServer() *soggy.Server {
     return 404, map[string]interface{} { "error": "Path not found" }
   })
   
-  webServer.Use(soggy.NewStaticServerMiddleware("/public"), &AppEngineMiddleware{}, webServer.Router)
+  webServer.Use(soggy.NewStaticServerMiddleware("/public"), &AppEngineWebMiddleware{}, webServer.Router)
   return webServer
 }
 
 func startApiServer() *soggy.Server {
   apiServer := soggy.NewServer("/api")
+  apiServer.Get("/me", ApiMe)
 
+  apiServer.Use(&AppEngineApiMiddleware{}, apiServer.Router)
   return apiServer
 }
 
 func startServer() {
   app := soggy.NewApp()
-  app.AddServers(startApiServer())
   app.AddServers(startWebServer())
+  app.AddServers(startApiServer())
   app.BindHandlers()
 }
 
